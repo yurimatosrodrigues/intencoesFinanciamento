b4a8f6d6673079bf06c319104db1086d70d2c800
Yuri Matos Rodrigues <yuri.matos@phibra.com.br>
2021-06-03 15:06:37 -0300

    Criacao do projeto

---
+++ src/main/java/br/gov/sp/etec/intencaoFinanciamento/IntencaoFinanciamentoApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.etec.intencaoFinanciamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntencaoFinanciamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntencaoFinanciamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/etec/intencaoFinanciamento/IntencaoFinanciamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.etec.intencaoFinanciamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntencaoFinanciamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
